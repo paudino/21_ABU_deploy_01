53029465d37654e11daaddf82e9203a4f81203a0
paudino <51296784+paudino@users.noreply.github.com>
2026-01-22 16:19:14 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+
+const CACHE_NAME = 'buon-umore-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/index.tsx',
+  'https://cdn-icons-png.flaticon.com/512/869/869869.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Strategia Stale-While-Revalidate
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+        });
+        return networkResponse;
+      });
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
